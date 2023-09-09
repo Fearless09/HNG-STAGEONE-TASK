8eb413da48df9d58bacf5b6ae7ebe8eff29ca46b
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-09-09 07:06:55 +0100

    Done

---
+++ main.js
@@ -0,0 +1,7 @@
+const daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+document.querySelector('#days').textContent = daysOfTheWeek[new Date().getDay() - 1]
+
+setInterval(() => {
+    document.querySelector('#time').textContent = `${new Date().getTime()}ms`
+}, 1);
